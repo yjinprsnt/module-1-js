16f37bcfc161672f4599fdece3725e07f4ca3e08
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2023-11-30 20:43:49 +0100

    add js

--- module-1-js/script.js
+++ module-1-js/script.js
@@ -3,11 +3,55 @@
 // const login = "mango935";
 // const isOnline = true;
 // const isAdmin = false;
+
 // const pricePerItem = 3500;
 // const orderedQuantity = 4;
+
 // const totalPrice = pricePerItem * orderedQuantity;
 // console.log(totalPrice);
+
 // const productName = "Droid";
 // const pricePerItem = 3500;
 // const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
 // console.log(message);
+
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+// console.log(message);
+
+// function sayHi() {
+//   console.log("Hello, this is my first function!");
+// }
+// sayHi("Function Declaration");
+
+// function add(a, b, c) {
+//   console.log(`Addition result equals ${a + b + c}`);
+// }
+
+// add(15, 27, 10);
+// add(10, 20, 30);
+// add(5, 10, 15);
+
+// function add(a, b, c) {
+//   // Change code below this line
+
+//   return a + b + c;
+//   // Change code above this line
+// }
+
+// add(2, 5, 8); // 15
+
+// console.log(add(15, 27, 10));
+// console.log(add(10, 20, 30));
+// console.log(add(5, 10, 15));
+
+// function makeMessage(name, price) {
+//   // Change code below this line
+//   const message = `You picked ${name}, price per item is ${price} credits`;
+//   // Change code above this line
+//   return message;
+// }
+
