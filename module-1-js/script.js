262740097f8f81d3206fb73c80a3446c2e6faffc
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2023-11-30 20:42:48 +0100

    Create script.js

---
+++ module-1-js/script.js
@@ -0,0 +1,13 @@
+// const topSpeed = 160;
+// const distance = 617.54;
+// const login = "mango935";
+// const isOnline = true;
+// const isAdmin = false;
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+// const totalPrice = pricePerItem * orderedQuantity;
+// console.log(totalPrice);
+// const productName = "Droid";
+// const pricePerItem = 3500;
+// const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
+// console.log(message);
