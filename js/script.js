31e2aa19803edf39576d8d1e106a98091b3dc0ee
Evgeniy Yurchenko <o6uga.ga@gmail.com>
2023-12-01 23:32:07 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -160,20 +160,20 @@
 //   return message;
 // }
 
-function checkStorage(available, ordered) {
-  let message;
-  // Change code below this line
+// function checkStorage(available, ordered) {
+//   let message;
+//   // Change code below this line
 
-  if (ordered === 0) {
-    message = 'There are no products in the order!';
-  } else if (ordered > available) {
-    message = 'Your order is too large, there are not enough items in stock!';
-  } else {
-    message = 'The order is accepted, our manager will contact you';
-  }
-  // Change code above this line
-  return message;
-}
+//   if (ordered === 0) {
+//     message = 'There are no products in the order!';
+//   } else if (ordered > available) {
+//     message = 'Your order is too large, there are not enough items in stock!';
+//   } else {
+//     message = 'The order is accepted, our manager will contact you';
+//   }
+//   // Change code above this line
+//   return message;
+// }
 
 // function isNumberInRange(start, end, number) {
 //   const isInRange = number >= start && number <= end; // Change this line
